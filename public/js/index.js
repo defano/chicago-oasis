6951d6fbf9108694c965791eb298028b5ac2644a
Matt DeFano <matt.defano@gmail.com>
2015-02-07 10:20:46 -0600

    Added awesome-checkbox component; added business markers selection elements

--- public/js/index.js
+++ public/js/index.js
@@ -1,77 +1,89 @@
 (function (index, $) {
 
-	var year = 2007;
-	var geo = "neighborhood";
-	var business = "Food";
+    var year = 2007;
+    var geo = "neighborhood";
+    var business = "Food";
 
-	function initGeoRadio() {
-		$('#neighborhood-radio').change(function() {
-			if ($(this).is(':checked')) {
-				geo = "neighborhood";
-				index.update();
-			}
-		});
+    function initGeoRadio() {
+        $('#neighborhood-radio').change(function () {
+            if ($(this).is(':checked')) {
+                geo = "neighborhood";
+                index.update();
+            }
+        });
 
-		$('#census-radio').change(function() {
-			if ($(this).is(':checked')) {
-				geo = "census";
-				index.update();
-			}
-		});
-	}
+        $('#census-radio').change(function () {
+            if ($(this).is(':checked')) {
+                geo = "census";
+                index.update();
+            }
+        });
+    }
 
-	function initBusinessMultiselect() {
-		$('#business-multiselect').multiselect({
-			buttonClass : 'desert-multiselect-button btn btn-primary',
-			enableFiltering : true,
-			filterBehavior : 'value',
-			enableCaseInsensitiveFiltering : true,
-			filterPlaceholder : 'Business Type',
-			includeSelectAllOption : true
-		});
-	}
+    function initBusinessMultiselect() {
+        $('#business-multiselect').multiselect({
+            buttonClass: 'desert-multiselect-button btn btn-primary',
+            enableFiltering: true,
+            filterBehavior: 'value',
+            enableCaseInsensitiveFiltering: true,
+            filterPlaceholder: 'Business Type',
+            includeSelectAllOption: true
+        });
+    }
 
-	function initYearSlider() {
+    function initYearSlider() {
 
-		// Continually update the label...
-		$("#year-slider").slider().on('slide', function(event) {
-			$("#year-value").text(event.value);
-		});
+        // Continually update the label...
+        $("#year-slider").slider().on('slide', function (event) {
+            $("#year-value").text(event.value);
+        });
 
-		// But only fire the update function once user has made a selection
-		$("#year-slider").slider().on('slideStop', function(event) {
-			$("#year-value").text(event.value);
-			year = event.value;
-			index.update();
-		});
-	}
+        // But only fire the update function once user has made a selection
+        $("#year-slider").slider().on('slideStop', function (event) {
+            $("#year-value").text(event.value);
+            year = event.value;
+            index.update();
+        });
+    }
 
-	function initBusinessDropdown() {
-		$(".dropdown-menu li a").click(
-				function() {
-					$("#business-dropdown").html(
-							$(this).text() + " <span class='caret'></span>");
-					business = $(this).text();
-					index.update();
-				});
-	}
+    function initBusinessDropdown() {
+        $(".dropdown-menu li a").click(
+            function () {
+                $("#business-dropdown").html(
+                    $(this).text() + " <span class='caret'></span>");
+                business = $(this).text();
+                index.update();
+            });
+    }
 
-	index.update = function() {
-		if (geo == "census") {
-			maps.showCensusTracts();
-		} else {
-			maps.showCommunities();
-		}
-		
-		console.log("Showing " + business + " across " + geo + " for year "
-				+ year);
-	};
+    function initBusinessMarkerRadios() {
+        $('#show-business-markers').change(function () {
+            if ($(this).is(":checked")) {
+                $("#critical-businesses").attr('disabled', false);
+                $("#all-businesses").attr('disabled', false);
+            } else {
+                $("#critical-businesses").attr('disabled', true);
+                $("#all-businesses").attr('disabled', true);
+            }
+        });
+    }
 
-	index.init = function() {    
-		initYearSlider();
-		initBusinessDropdown();
-		initGeoRadio();
-		initBusinessMultiselect();
-	};
+    index.update = function () {
+        if (geo == "census") {
+            maps.showCensusTracts();
+        } else {
+            maps.showCommunities();
+        }
+
+        console.log("Showing " + business + " across " + geo + " for year " + year);
+    };
+
+    index.init = function () {
+        initBusinessMarkerRadios();
+        initYearSlider();
+        initBusinessDropdown();
+        initGeoRadio();
+        initBusinessMultiselect();
+    };
 
 }(window.index = window.index || {}, jQuery));
