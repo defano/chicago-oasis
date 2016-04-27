b4b9a64e6d512a3c8168a28a17114a699573b333
Matt DeFano <matt@defano.com>
2016-04-27 12:57:06 -0500

    Added ability to specify jade template for markdown requests

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var marked = require('marked');
+var url = require('url')
+
 
 app.set('views', __dirname + '/public/jade');
 app.set('port', (process.env.PORT || 5000));
@@ -11,11 +13,13 @@ app.use(express.static(__dirname + '/public'));
 app.use('/lib', express.static(__dirname + '/bower_components'));
 
 app.get('/*.md', function (req, res) {
+    var query = url.parse(req.url, true).query;
     var path = __dirname + '/md/' + req.path;
     var file = fs.readFileSync(path, 'utf8');
+    var template = "template" in query ? query["template"] : "markdown" 
 
     // I'm sure there's a more clever way to do this...
-    res.render('markdown', {
+    res.render(template, {
         markdown: marked(file)
     });
 });
